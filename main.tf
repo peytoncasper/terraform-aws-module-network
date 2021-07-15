e54a8affaf51764d2ff15342a4e18dd7ff18591f
Peyton Casper <peytoncas@gmail.com>
2021-07-15 18:09:15 -0400

    Added subnet and subnet id output

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_subnet" "vm_subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "us-east-1a"
+}
